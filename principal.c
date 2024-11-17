b29a883ce3bd23b2e66c158805e55a8ffac1a78d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-17 18:09:03 -0300

    Nome e cargo do funcionário na entrada de dados

--- principal.c
+++ principal.c
@@ -1,7 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
 int main() {
 	char nome[50];
 	char cargo[50];
 	char matricula[50];
 
-	
+	printf("Digite o nome completo do funcionario: ");
+	fgets(nome, sizeof(nome), stdin);
+
+	nome[strcspn(nome, "\n")] = '\0';
+
+	printf("Digite o cargo do %s: ", nome);
+	fgets(cargo, sizeof(cargo), stdin);
+
+	printf("%s\n", nome);
+	printf("%s\n", cargo);
 }
