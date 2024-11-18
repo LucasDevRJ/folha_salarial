c8c216f4c7c2fc2821ebadb4fbe29c2f704749dd
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-17 21:44:35 -0300

    Decorando

--- principal.c
+++ principal.c
@@ -11,9 +11,9 @@ int main() {
 	int quantidade_horas_extras;
 	double salario_base;
 	double salario_bruto;
-	double total_hora_extra;
 	double valor_hora = salario_base / media_horas_trabalhadas_mes;
 	double valor_hora_extra = valor_hora + (50 / 100);
+	double total_hora_extra = quantidade_horas_extras * valor_hora_extra;
 
 	printf("Digite o nome completo do funcionario: ");
 	fgets(nome, sizeof(nome), stdin);
@@ -31,17 +31,16 @@ int main() {
 	printf("Digite a quantidade de horas extras exercidas: ");
 	scanf("%d", &horas_extras);
 
-
 	int segundos = time(0);
 	srand(segundos);
 	int numero_aleatorio = rand();
 	matricula = numero_aleatorio % 99999;
 
-	printf("----------|FOLHA SALARIAL|----------\n");
+	printf("--------------------|FOLHA SALARIAL|--------------------\n");
 	printf("Nome: %s\n", nome);
 	printf("Cargo: %s\n", cargo);
 	printf("Matricula: %d\n", matricula);
-	printf("Salario: %d\n", salario_base);
-	printf("Horas extras: %d\n", horas_extras);
-	printf("-----------------------------------\n");
+	printf("Salario Base: %d\n", salario_base);
+	printf("Quantidade de horas extras: %d\n", horas_extras);
+	printf("--------------------------------------------------------\n");
 }
