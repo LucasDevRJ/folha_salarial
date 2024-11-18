fb5d00f8ca3846c35266238da223fc89ddbb7610
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-18 16:50:31 -0300

    Calculos de horas noturnas

--- principal.c
+++ principal.c
@@ -7,13 +7,16 @@ int main() {
 	char nome[50];
 	char cargo[50];
 	int matricula;
-	int media_horas_trabalhadas_mes = 220;
-	int quantidade_horas_extras;
 	double salario_base;
 	double salario_bruto;
 	double valor_hora;
+	int media_horas_trabalhadas_mes = 220;
+	int quantidade_horas_extras;
 	double valor_hora_extra;
 	double total_hora_extra;
+	int quantidade_horas_noturnas;
+	double valor_hora_noturna;
+	double total_hora_noturna;
 
 	printf("Digite o nome completo do funcionario: ");
 	fgets(nome, sizeof(nome), stdin);
@@ -31,20 +34,32 @@ int main() {
 	printf("Digite a quantidade de horas extras exercidas: ");
 	scanf("%d", &quantidade_horas_extras);
 
+	printf("Digite a quantidade de horas noturnas exercidas: ");
+	scanf("%d", &quantidade_horas_noturnas);
+
+	//Lógica para gerar números aleatorios para a matrícula do funcionário
 	int segundos = time(0);
 	srand(segundos);
 	int numero_aleatorio = rand();
 	matricula = numero_aleatorio % 99999;
 
 	valor_hora = salario_base / media_horas_trabalhadas_mes;
+
 	valor_hora_extra = valor_hora + (valor_hora * 0.50);
 	total_hora_extra = quantidade_horas_extras * valor_hora_extra;
+
+	valor_hora_noturna = valor_hora + (valor_hora * 0.20);
+	total_hora_noturna = quantidade_horas_noturnas * valor_hora_noturna;
+
+	salario_bruto = salario_base + total_hora_extra + total_hora_noturna;
 
 	printf("--------------------|FOLHA SALARIAL|--------------------\n");
 	printf("Nome: %s\n", nome);
 	printf("Cargo: %s\n", cargo);
 	printf("Matricula: %d\n", matricula);
-	printf("Salario Base: %.2lf\n", salario_base);
-	printf("Horas Extras: %.2lf\n", total_hora_extra);
+	printf("Salario Base: R$ %.2lf\n", salario_base);
+	printf("Horas Extras: R$ %.2lf\n", total_hora_extra);
+	printf("Horas Noturnas: R$ %.2lf\n", total_hora_noturna);
+	printf("Salario Bruto: R$ %.2lf\n", salario_bruto);
 	printf("--------------------------------------------------------\n");
 }
