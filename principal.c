2ec0575dd5cbeaede59b2f528d9e51ec4dde1314
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-19 19:04:10 -0300

    Consertando lógica do INSS e IRRF

--- principal.c
+++ principal.c
@@ -10,6 +10,7 @@ int main() {
 	int matricula;
 	double salario_base;
 	double salario_bruto;
+	double salario_liquido;
 	double valor_hora;
 	int media_horas_trabalhadas_mes = 220;
 	int quantidade_horas_extras;
@@ -27,6 +28,11 @@ int main() {
 	double percentual_inss;
 	double valor_inss;
 	double valor_total_inss;
+	double base_calculo_irrf;
+	double percentual_irrf;
+	double deducao_fixa;
+	double valor_irrf;
+	double valor_total_irrf;
 
 	printf("Digite o nome completo do funcionario: ");
 	fgets(nome, sizeof(nome), stdin);
@@ -77,37 +83,99 @@ int main() {
 
 	comissao = valor_total_vendas * percentual_comissao;
 
-	bonificacao = salario_base * 0.10;
+	bonificacao = 0;
 
 	salario_bruto = salario_base + total_hora_extra + total_hora_noturna + comissao + bonificacao;
 
+	//double casas_decimais = pow(NUMERO_DESLOCAMENTO, NUMERO_CASAS_DECIMAIS);
+
 	if (salario_bruto <= 1320.00 || salario_bruto > 1320.00) {
 		percentual_inss = 7.5 / 100;
-		valor_inss = 1320.00 * percentual_inss;
-		printf("%.2lf\n", valor_inss); 
+		double maior_valor = salario_bruto < 1320.00 ? salario_bruto : 1320.00;
+		valor_inss = maior_valor * percentual_inss; 
+		//printf("Percentual INSS: %lf\n", percentual_inss);
+		printf("INSS Faixa 1 R$ %.2lf\n", valor_inss);
 		valor_total_inss += valor_inss;
 	} 
 
-	if (salario_bruto >= 1320.01 || salario_bruto <= 2571.29) {
+	if (salario_bruto >= 1320.01) {
 		percentual_inss = 9.0 / 100;
-		valor_inss = fabs((1320.01 - 2571.29)) * percentual_inss; 
-		printf("%.2lf\n", valor_inss); 
+		//printf("Percentual INSS: %lf\n", percentual_inss);
+		double maior_valor = salario_bruto < 2571.29 ? salario_bruto : 2571.29;
+		valor_inss = fabs((1320.01 - maior_valor)) * percentual_inss; 
+		//valor_inss = round(valor_inss * 100) / 100;
+		//valor_inss = floor(valor_inss * casas_decimais) / casas_decimais;
+		printf("INSS Faixa 2 R$ %.2lf\n", valor_inss);
 		valor_total_inss += valor_inss;
 	}
 
-	if (salario_bruto >= 2571.30 || salario_bruto <= 3856.94) {
+	if (salario_bruto >= 2571.30) {
 		percentual_inss = 12.0 / 100;
-		valor_inss = fabs((2571.30 - 3856.94)) * percentual_inss; 
-		printf("%.2lf\n", valor_inss); 
+		//printf("Percentual INSS: %lf\n", percentual_inss);
+		double maior_valor = salario_bruto < 3856.94 ? salario_bruto : 3856.94;
+		valor_inss = fabs((2571.29 - maior_valor)) * percentual_inss; 
+		//valor_inss = floor(valor_inss * casas_decimais) / casas_decimais;
+		printf("INSS Faixa 3 R$ %.2lf\n", valor_inss);
 		valor_total_inss += valor_inss;
 	} 
 
-	if (salario_bruto >= 3856.95 || salario_bruto <= 5000.00) {
+	if (salario_bruto >= 3856.95) {
 		percentual_inss = 14.0 / 100;
-		valor_inss = fabs((3856.95 - 5000.00)) * percentual_inss; 
-		printf("%.2lf\n", valor_inss); 
+		//printf("Percentual INSS: %lf\n", percentual_inss);
+		double maior_valor = salario_bruto < 7507.49 ? salario_bruto : 7507.49;
+		valor_inss = fabs((3856.95 - maior_valor)) * percentual_inss; 
+		//valor_inss = floor(valor_inss * casas_decimais) / casas_decimais;
+		printf("INSS Faixa 4 R$ %.2lf\n", valor_inss);
 		valor_total_inss += valor_inss;
 	}
+
+	//valor_total_inss = round(valor)
+
+	base_calculo_irrf = salario_bruto - valor_total_inss;
+	printf("Base de calculo IRRF: R$ %.2lf\n", base_calculo_irrf);
+
+	if (base_calculo_irrf <= 2112.00) {
+		percentual_irrf = 0.0;
+		deducao_fixa = 0.0;
+	}
+	//2736.94
+	if (base_calculo_irrf >= 2112.01) {
+		percentual_irrf = 7.5 / 100;
+		double maior_valor = base_calculo_irrf < 2826.65 ? base_calculo_irrf : 2826.65;
+		valor_irrf = fabs((2112.01 - maior_valor)) * percentual_irrf; 
+		deducao_fixa = 158.40;
+		printf("IRRF Faixa 2 R$ %.2lf\n", valor_irrf);
+		valor_total_irrf += valor_irrf;
+	}
+
+	if (base_calculo_irrf >= 2826.66) {
+		percentual_irrf = 15.0 / 100;
+		double maior_valor = base_calculo_irrf < 3751.05 ? base_calculo_irrf : 3751.05;
+		valor_irrf = fabs((2826.66 - maior_valor)) * percentual_irrf; 
+		deducao_fixa = 370.40;
+		printf("IRRF Faixa 3 R$ %.2lf\n", valor_irrf);
+		valor_total_irrf += valor_irrf;
+	}
+
+	if (base_calculo_irrf >= 3751.06) {
+		percentual_irrf = 22.5 / 100;
+		double maior_valor = base_calculo_irrf < 4664.68 ? base_calculo_irrf : 4664.68;
+		valor_irrf = fabs((3751.06 - maior_valor)) * percentual_irrf; 
+		deducao_fixa = 651.73;
+		printf("IRRF Faixa 4 R$ %.2lf\n", valor_irrf);
+		valor_total_irrf += valor_irrf;
+	}
+
+	if (base_calculo_irrf > 4664.68) {
+		percentual_irrf = 27.5 / 100;
+		double maior_valor = base_calculo_irrf < 4664.68 ? base_calculo_irrf : 4664.68;
+		valor_irrf = fabs((4664.68 - maior_valor)) * percentual_irrf; 
+		printf("IRRF Faixa 5 R$ %.2lf\n", valor_irrf);
+		valor_total_irrf += valor_irrf;
+		deducao_fixa = 884.96;
+	}
+
+	valor_total_irrf -= deducao_fixa;
 
 	printf("--------------------|FOLHA SALARIAL|--------------------\n");
 	printf("Nome: %s\n", nome);
@@ -119,6 +187,7 @@ int main() {
 	printf("Comissao: R$ %.2lf\n", comissao);
 	printf("Bonificacao: R$ %.2lf\n", bonificacao);
 	printf("INSS: R$ %.2lf\n", valor_total_inss);
+	printf("IRRF: R$ %.2lf\n", valor_total_irrf);
 	printf("Salario Bruto: R$ %.2lf\n", salario_bruto);
 	printf("--------------------------------------------------------\n");
 }
